16b3b615c514e77d7b01f6543d449d0254b485c3
QuicklyDG <quicklydeguzman@gmail.com>
2026-09-03 09:02:43 +0800

    adjust GTM

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-full flex flex-col">
         {children}
 
-
+       
         {/* Google Tag Manager */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
