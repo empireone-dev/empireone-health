0b8f7e6f81c6ddb287efe62e570a00bfdece6b64
QuicklyDG <quicklydeguzman@gmail.com>
2026-09-04 15:18:27 +0800

    change google tag manager

--- app/layout.js
+++ app/layout.js
@@ -30,20 +30,17 @@ export default function RootLayout({ children }) {
       <body className="min-h-full flex flex-col">
         {children}
 
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-69S13CLQFC"
-          strategy="afterInteractive"
-        />
-
-        <Script id="google-analytics" strategy="afterInteractive">
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-69S13CLQFC');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WMZK6S7G');
           `}
         </Script>
+        {/* End Google Tag Manager */}
       </body>
     </html>
   );
